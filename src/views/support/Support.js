b3130efcb263f93975e1c59643c8fddaa96f6c8b
Guilherme-Aguiar <gui9627oli@gmail.com>
2021-04-18 02:07:43 -0300

    finishes implementation of the support functionality

--- src/views/support/Support.js
+++ src/views/support/Support.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   CButton,
   CCard,
@@ -14,9 +14,47 @@ import {
   CRow
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-
+import emailjs from 'emailjs-com';
 
 const Support = () => {
+
+  const [subject, setSubject] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleSubjectChange = event => {
+    event.preventDefault();
+    setSubject(event.target.value);
+  }
+
+  const handleContentChange = event => {
+    event.preventDefault();
+    setContent(event.target.value);
+  }
+
+  const sendEmail = () => {
+    const templateId = 'template_ylswnko';
+    const replyEmail = 'windrisapp@gmail.com'
+
+    sendFeedback(templateId, {message: content, from_name: 'usuario', to_name: 'adm', reply_to: replyEmail})
+  }
+
+  const sendFeedback = (templateId, variables) => {
+    emailjs.send(
+      'service_92uu25u', templateId, 
+      variables, 'user_Z5ragRN1iflFMiK6XYAUe'
+      ).then(res => {
+        console.log('Email successfully sent!')
+      })
+      // Handle errors here however you like, or use a React error boundary
+      .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
+    }
+
+  const resetEmail = () => {
+    setSubject('');
+    setContent('');
+  }
+
+
   return (
     <>
       <CRow>
@@ -32,7 +70,12 @@ const Support = () => {
                     <CLabel htmlFor="text-input">Assunto</CLabel>
                   </CCol>
                   <CCol xs="12" md="9">
-                    <CInput id="text-input" name="text-input" placeholder="Escreva aqui o assunto" />
+                    <CInput 
+                      onChange={handleSubjectChange} 
+                      value={subject} lazy={1} 
+                      id="text-input" 
+                      name="text-input" 
+                      placeholder="Escreva aqui o assunto" />
                   </CCol>
                 </CFormGroup>
                 <CFormGroup row>
@@ -40,7 +83,9 @@ const Support = () => {
                     <CLabel htmlFor="textarea-input">Título</CLabel>
                   </CCol>
                   <CCol xs="12" md="9">
-                    <CTextarea 
+                    <CTextarea
+                    onChange={handleContentChange} 
+                    value={content} lazy={1} 
                       name="textarea-input" 
                       id="textarea-input" 
                       rows="9"
@@ -51,8 +96,8 @@ const Support = () => {
               </CForm>
             </CCardBody>
             <CCardFooter>
-              <CButton type="submit" size="sm" color="primary"><CIcon name="cil-scrubber" /> Submit</CButton>
-              <CButton type="reset" size="sm" color="danger"><CIcon name="cil-ban" /> Reset</CButton>
+              <CButton onClick={sendEmail} type="submit" size="sm" color="primary"><CIcon name="cil-scrubber" /> Submit</CButton>
+              <CButton onClick={resetEmail} type="reset" size="sm" color="danger"><CIcon name="cil-ban" /> Reset</CButton>
             </CCardFooter>
           </CCard>
         </CCol>
