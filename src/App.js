86f658109cef1ab4d95627ce4d83244c28ed347e
Guilherme-Aguiar <gui9627oli@gmail.com>
2021-04-24 17:04:33 -0300

    Revert "Create component and update docker-compose"
    This reverts commit 011615b6ff858a963387f6879d98ca4737b0995f.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ const Login = React.lazy(() => import('./views/pages/login/Login'));
 const Register = React.lazy(() => import('./views/pages/register/Register'));
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
-const Windris = React.lazy(() => import('./views/pages/windris/windris'));
 
 class App extends Component {
 
@@ -29,7 +28,6 @@ class App extends Component {
               <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
               <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
               <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
-              <Route exact path="/windris" name="Windris" render={props => <Windris {...props}/>} />
               <Route path="/" name="Home" render={props => <TheLayout {...props}/>} />
             </Switch>
           </React.Suspense>
